27c81aaba4a2b23c85b20f3e205fcceea553c36a
b1b2b3b4b5b6 <a1439458305@163.com>
2020-12-15 06:33:13 +0000

    first commit

---
+++ python/main.py
@@ -0,0 +1,10 @@
+'''
+Author: your name
+Date: 2020-12-15 06:24:50
+LastEditTime: 2020-12-15 06:25:29
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /template/main.py
+'''
+
+print("python")
