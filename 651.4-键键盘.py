5874c674194c2821802aeb7884c83de71a71171c
b1b2b3b4b5b6 <a1439458305@163.com>
2021-05-10 00:46:24 +0000

    1

--- 651.4-键键盘.py
+++ 651.4-键键盘.py
@@ -16,23 +16,6 @@ FilePath: /leetcode/651.4-键键盘.py
 # @lc code=start
 
 
-# 算法复杂度：n^2
-# 空间复杂度：n
-# 会超时
-
-class Solution:
-    def maxA(self, N: int) -> int:
-        dp = [0] * (N + 1)
-
-        for i in range(1, N + 1):
-            dp[i] = dp[i - 1] + 1
-            for j in range(2, i + 1):
-                dp[i] = max(dp[i], dp[j - 2] * (i - j + 1))
-
-        return dp[N]
-        # @lc code=end
-
-
 '''
 状态：
     屏幕A的数量，剩余的按键数n,缓冲区A数量m
@@ -48,29 +31,43 @@ base_case：
 分析可得：key2，key3只有连用才有意义，故合并
 
 进一步考虑：
-    最优操作必然先输入若干个A，然后全选复制，若干粘贴，全选复制，若干粘贴
-    贪心法
+    贪心法(不会)：最优操作必然先输入若干个A，然后全选复制，若干粘贴，全选复制，若干粘贴
+    过滤一些情况
 '''
 
 # 算法复杂度：n^3
 # 空间复杂度：n^3
 # 会超时
 
-# class Solution:
-#     def maxA(self, N: int) -> int:
-#         memo = {}
 
-#         def dp(i, n, m):
-#             if n <= 0:
-#                 return i
-#             if (i, n, m) in memo:
-#                 return memo[i, n, m]
-#             sel = max(
-#                 dp(i + 1, n - 1, m),
-#                 dp(i, n - 2, i),
-#                 dp(i + m, n-1, m)
-#             )
-#             memo[i, n, m] = sel
-#             return sel
+class Solution:
+    def maxA(self, N: int) -> int:
+        memo = {}
 
-#         return dp(0, N, 0)
+        def dp(i, n, m):
+            if n <= 0:
+                return i
+            if (i, n, m) in memo:
+                return memo[i, n, m]
+
+            if i > 8:
+                max1 = - float('INF')
+            else:
+                max1 = dp(i + 1, n - 1, m)
+            if m >= i:
+                max2 = - float('INF')
+            else:
+                max2 = dp(i, n - 2, i)
+            if m <= 0:
+                max3 = - float('INF')
+            else:
+                max3 = dp(i + m, n - 1, m)
+            sel = max(
+                max1,
+                max2,
+                max3
+            )
+            memo[i, n, m] = sel
+            return sel
+
+        return dp(0, N, 0)
