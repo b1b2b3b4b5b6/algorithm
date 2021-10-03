1c1b97d5dced8ff4cfb3b74bf4b948555b34f15c
b1b2b3b4b5b6 <a1439458305@163.com>
2021-10-03 19:13:44 +0800

    done

--- python/887.鸡蛋掉落.py
+++ python/887.鸡蛋掉落.py
@@ -14,54 +14,114 @@
 # Testcase Example:  '1\n2'
 #
 # 给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
-# 
+#
 # 已知存在楼层 f ，满足 0  ，任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。
-# 
+#
 # 每次操作，你可以取一枚没有碎的鸡蛋并把它从任一楼层 x 扔下（满足 1
 # ）。如果鸡蛋碎了，你就不能再次使用它。如果某枚鸡蛋扔下后没有摔碎，则可以在之后的操作中 重复使用 这枚鸡蛋。
-# 
+#
 # 请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少？
-# 
-# 
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：k = 1, n = 2
 # 输出：2
 # 解释：
-# 鸡蛋从 1 楼掉落。如果它碎了，肯定能得出 f = 0 。 
-# 否则，鸡蛋从 2 楼掉落。如果它碎了，肯定能得出 f = 1 。 
-# 如果它没碎，那么肯定能得出 f = 2 。 
-# 因此，在最坏的情况下我们需要移动 2 次以确定 f 是多少。 
-# 
-# 
+# 鸡蛋从 1 楼掉落。如果它碎了，肯定能得出 f = 0 。
+# 否则，鸡蛋从 2 楼掉落。如果它碎了，肯定能得出 f = 1 。
+# 如果它没碎，那么肯定能得出 f = 2 。
+# 因此，在最坏的情况下我们需要移动 2 次以确定 f 是多少。
+#
+#
 # 示例 2：
-# 
-# 
+#
+#
 # 输入：k = 2, n = 6
 # 输出：3
-# 
-# 
+#
+#
 # 示例 3：
-# 
-# 
+#
+#
 # 输入：k = 3, n = 14
 # 输出：4
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
-# 1 
-# 1 
-# 
-# 
+#
+#
+# 1
+# 1
+#
+#
 #
 
 # @lc code=start
+
+'''
+最坏情况的最少次数
+
+最坏:碎了和没碎的最坏情况
+最少:在最坏前提下的最优解
+
+
+状态:dp(left_k, left_n)
+    选择:
+        遍历1-i扔鸡蛋,取最小
+            碎了:dp(left_k-1, i-1) + 1
+            没碎:dp(left_k, left_n - i) + 1
+base_case:
+    left_k = 1:
+        return left_n
+    left_n = 0:
+        return 0
+
+动态规划+备忘录 会超时(将遍历转为二分法求凹点)
+'''
+
+
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-# @lc code=end
+        memo = {}
 
+        def dp(left_k, left_n):
+            if left_k == 1:
+                return left_n
+            if left_n == 0:
+                return 0
+
+            if (left_k, left_n) in memo:
+                return memo[(left_k, left_n)]
+
+            res = float('INF')
+
+            start = 1
+            end = left_n
+
+            while end >= start:
+                mid = (end + start)//2
+                broken = dp(left_k-1, mid-1) + 1
+                not_broken = dp(left_k, left_n - mid) + 1
+
+                if broken > not_broken:
+                    end = mid - 1
+                    res = min(res, broken)
+                else:
+                    start = mid + 1
+                    res = min(res, not_broken)
+            memo[(left_k, left_n)] = res
+
+            return res
+
+        return dp(k, n)
+
+
+'''
+
+'''
+
+# @lc code=end
